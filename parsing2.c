73aca10842acedc1cfdba082bae8e4107aa45602
Pierre LEVEQUE <pleveque@e2r3p5.clusters.42paris.fr>
2022-02-04 12:24:58 +0100

    jngjeropjhdtnjilnjiogdflnjjingf

--- parsing2.c
+++ parsing2.c
@@ -6,7 +6,7 @@
 /*   By: pleveque <pleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 17:48:24 by pleveque          #+#    #+#             */
-/*   Updated: 2022/02/03 17:59:45 by pleveque         ###   ########.fr       */
+/*   Updated: 2022/02/04 12:23:45 by pleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 float	*get_ratio(t_screen *s, float *ratio)
 {
-	ratio[0] = tern(s->map_x > s->map_y,
-			(float)s->map_y / (float)s->map_x, 1);
-	ratio[1] = tern(s->map_x > s->map_y,
-			1, (float)s->map_x / (float)s->map_y);
+	ratio[0] = 1;
+	ratio[1] = 1;
+	if (s->map_x > s->map_y)
+		ratio[1] = (float)s->map_y / (float)s->map_x;
+	else
+		ratio[0] = (float)s->map_x / (float)s->map_x;
 	return (ratio);
 }
